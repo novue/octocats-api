671c1206e0425063d47cc149c3947214176f6337
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-01 18:34:02 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.NODE_ENV === 'production' ?  80 : 3000;
+const DEFAULT_PAGE = 1;
+const DEFAULT_PAGE_SIZE = 10;
+const OCTOCATS = JSON.parse(fs.readFileSync('./octocats.json').toString());
+
+app.use(cors());
+
+app.get('/api/octocats', function(req, res) {
+  let page = parseInt(req.query.page) || DEFAULT_PAGE;
+  let size = parseInt(req.query.pageSize) || DEFAULT_PAGE_SIZE;
+  res.json({
+    data: OCTOCATS.slice((page - 1) * size, page * size),
+    pagination: {
+      page: page,
+      total: OCTOCATS.length,
+      pageSize: size,
+      hasNextPage: page * size < OCTOCATS.length
+    }
+  });
+});
+
+app.get('*', function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, function() {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+
