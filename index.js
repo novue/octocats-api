4db89a0b797e886a9e8ba1a5cb7b69c12de8eb52
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-20 14:14:44 +0800

    CORS

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const fs = require('fs');
-const cors = require('cors');
-const express = require('express');
+const fs = require('fs')
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.NODE_ENV === 'production' ?  80 : 3000;
-const DEFAULT_PAGE = 1;
-const DEFAULT_PAGE_SIZE = 10;
-const OCTOCATS = JSON.parse(fs.readFileSync('./octocats.json').toString());
+const PORT = process.env.NODE_ENV === 'production' ?  80 : 3000
+const DEFAULT_PAGE = 1
+const DEFAULT_PAGE_SIZE = 10
+const OCTOCATS = JSON.parse(fs.readFileSync('./octocats.json').toString())
 
 function cats(req, res) {
-  let page = parseInt(req.query.page) || DEFAULT_PAGE;
-  let size = parseInt(req.query.pageSize) || DEFAULT_PAGE_SIZE;
+  let page = parseInt(req.query.page) || DEFAULT_PAGE
+  let size = parseInt(req.query.pageSize) || DEFAULT_PAGE_SIZE
   return {
     data: OCTOCATS.slice((page - 1) * size, page * size),
     pagination: {
@@ -21,25 +20,34 @@ function cats(req, res) {
       totalItems: OCTOCATS.length,
       totalPages: Math.ceil(OCTOCATS.length / size)
     }
-  };
+  }
 }
 
 app.get('/jsonp/octocats', function(req, res) {
-  res.jsonp(cats(req, res));
-});
+  res.jsonp(cats(req, res))
+})
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.header('origin') || '*')
+  res.header('Access-Control-Max-Age', 86400)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS')
+  next()
+})
 
 app.get('/api/octocats', function(req, res) {
-  res.json(cats(req, res));
-});
+  res.json(cats(req, res))
+})
+
+app.put('/api/octocats/:id', function(req, res) {
+  res.json({ id: req.params.id })
+})
 
 app.get('*', function(req, res) {
-  res.sendStatus(404);
-});
+  res.sendStatus(404)
+})
 
 app.listen(PORT, function() {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  console.log(`Server is listening on port ${PORT}`)
+})
 
 
